d1ca6b02ad37a4397781576f03bc57e00a91dcd6
Poojitha V <poojithav462@gmail.com>
2026-04-24 17:42:08 +0530

    Implement custom topology with Mininet

---
+++ topology.py
@@ -0,0 +1,38 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import OVSController
+from mininet.cli import CLI
+
+
+class MyTopo(Topo):
+    def build(self):
+        s1 = self.addSwitch('s1')
+
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+
+
+topo = MyTopo()
+net = Mininet(topo=topo, controller=OVSController)
+
+net.start()
+
+# Initial connectivity
+net.pingAll()
+
+# Link failure
+net.configLinkStatus('s1', 'h1', 'down')
+net.pingAll()
+
+# Link recovery
+net.configLinkStatus('s1', 'h1', 'up')
+net.pingAll()
+
+CLI(net)
+
+net.stop()
